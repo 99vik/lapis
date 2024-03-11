86ea70883c0a540db5760d30428e37685aefe468
Viktor Miric <viktorm47@gmail.com>
2024-03-11 19:55:03 +0100

    Transparent navbar on top of the page

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,12 +1,35 @@
+'use client';
+
 import Image from 'next/image';
 import Link from 'next/link';
 import { NavbarMenu } from './NavbarMenu';
 import MobileMenu from './MobileMenu';
+import { useEffect, useState } from 'react';
+import { cn } from '@/lib/utils';
 
 export default function Navbar() {
+  const [transparent, setTransparent] = useState(true);
+
+  useEffect(() => {
+    function scrollCheck() {
+      window.scrollY >= 80 ? setTransparent(false) : setTransparent(true);
+    }
+    window.addEventListener('scroll', scrollCheck);
+  }, []);
+
   return (
-    <nav className="w-full sticky top-0 bg-background/60 border-b z-50">
-      <div className="absolute -z-40 w-full h-full bg-transparent backdrop-blur" />
+    <nav
+      className={cn(
+        'w-full sticky top-0  z-50 transition',
+        transparent ? 'bg-background/0' : 'bg-background/60 border-b'
+      )}
+    >
+      <div
+        className={cn(
+          'absolute -z-40 w-full h-full bg-transparent transition',
+          transparent ? 'backdrop-blur-none' : 'backdrop-blur'
+        )}
+      />
       <div className="flex mx-auto max-w-screen-2xl items-center justify-between px-10 md:px-16 py-4">
         <Link href="/">
           <div className="flex items-center gap-2">
