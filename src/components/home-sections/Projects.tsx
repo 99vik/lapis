c708c8854c992e2e342b20d1f44bc97c498dd552
Viktor Miric <viktorm47@gmail.com>
2024-03-12 20:03:16 +0100

    Infinite moving projects cards

--- src/components/home-sections/Projects.tsx
+++ src/components/home-sections/Projects.tsx
@@ -3,30 +3,30 @@ import { InfiniteMovingCards } from '../ui/infinite-moving-cards';
 export default function Projects() {
   const items = [
     {
-      body: 'teststring',
+      body: 'AMICORC will analyze recent research findings in human cognition, cognitive robotics and human robot interaction, and use them as the basis for developing new robot reasoning and interaction strategies. Computational architecture developed in AMICORC could be seen as context-to-data interpreter that endow machines to “reason” based on constantly changing perspectives.',
       title: 'AMICORC Project',
     },
     {
-      body: 'teststrinasdasdg',
+      body: 'Cartesian coordinate robot is a part of the student work where all components are joint together manually and programmed. We use information visualization techniques to additionally control and program the robot. This work is supported by ProEL and RoboDK.',
       title: 'Cartesian coordinate robot project',
     },
     {
-      body: 'testsasdasdasdtring',
-      title: 'Bodily awareness robot control and virtual reality',
+      body: 'Focus of this work is an approach in which a robot is using a virtual or inner representation of its own body to ensure a safe and efficient interaction within the physical world. By analyzing correlations within its surroundings, the robot is able to plan task-oriented and socially accepting responses. This approach is inspired by the phenomenon called bodily awareness, which is found in humans.',
+      title: 'Bodily awareness control and virtual reality',
     },
     {
-      body: 'teststasdasdring',
+      body: 'Focus of this work is an approach in which a robot is using a virtual or inner representation of its own body to ensure a safe and efficient interaction within the physical world. By analyzing correlations within its surroundings, the robot is able to plan task-oriented and socially accepting responses. This approach is inspired by the phenomenon called bodily awareness, which is found in humans.',
       title: 'Mixed reality applications',
     },
     {
-      body: 'testsasdasdsadtring',
+      body: 'Projects of our students text',
       title: 'Student projects',
     },
   ];
   return (
-    <div className="flex flex-col overflow-hidden rounded-3xl h-screen bg-zinc-900/40 border mb-10 gap-10 items-center py-10 px-4 sm:px-6 md:w-[80%] max-w-screen-xl mx-auto">
+    <div className="flex flex-col overflow-hidden rounded-3xl bg-zinc-900/40 border mb-10 gap-10 items-center py-10 px-4 sm:px-6 md:w-[80%] max-w-screen-xl mx-auto">
       <h2 className="text-3xl font-bold mb-2">Our projects</h2>
-      <InfiniteMovingCards items={items} speed="fast" />
+      <InfiniteMovingCards items={items} speed="slow" />
     </div>
   );
 }
--- src/components/ui/infinite-moving-cards.tsx
+++ src/components/ui/infinite-moving-cards.tsx
@@ -2,6 +2,7 @@
 
 import { cn } from '@/lib/utils';
 import React, { useEffect, useState } from 'react';
+import { Button } from './button';
 
 export const InfiniteMovingCards = ({
   items,
@@ -86,28 +87,31 @@ export const InfiniteMovingCards = ({
       >
         {items.map((item, idx) => (
           <li
-            className="w-[350px] max-w-full relative rounded-2xl border border-b-0 flex-shrink-0 border-slate-700 px-8 py-3 md:w-[450px]"
+            className="w-[350px] max-w-full relative rounded-2xl border border-b-0 flex-shrink-0 border-slate-700 px-8 py-4 md:w-[450px]"
             style={{
               background:
-                'linear-gradient(180deg, var(--neutral-400), var(--neutral-500)',
+                'linear-gradient(180deg, var(--white), var(--neutral-100)',
             }}
             key={item.title}
           >
-            <div>
+            <div className="h-full">
               <div
                 aria-hidden="true"
                 className="user-select-none -z-1 pointer-events-none absolute -left-0.5 -top-0.5 h-[calc(100%_+_4px)] w-[calc(100%_+_4px)]"
               ></div>
 
-              <div className="relative z-20 flex flex-col items-start justify-center">
+              <div className="relative flex-1 z-20 h-full flex flex-col gap-3 items-start justify-between">
                 <span className="">
-                  <span className=" text-sm leading-[1.6] text-gray-400 font-normal">
+                  <span className=" text-xl leading-[1.6] text-black font-semibold">
                     {item.title}
                   </span>
                 </span>
-                <span className=" relative z-20 text-sm leading-[1.6] text-gray-100 font-normal">
+                <span className="relative h-full z-20 text-sm leading-[1.6] text-neutral-600 font-normal">
                   {item.body}
                 </span>
+                <Button className="w-full bg-neutral-900 hover:bg-neutral-700 text-white">
+                  View more
+                </Button>
               </div>
             </div>
           </li>
