a2e0e89c0103316d230bd02bf6753ecaf7498b13
Viktor Miric <viktorm47@gmail.com>
2024-03-16 00:03:33 +0100

    About us 3d location pin

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,9 +1,11 @@
+import { LocationPin } from '@/components/LocationPin';
 import { SparklesTitle } from '@/components/SparklesTitle';
 
 export default function Page() {
   return (
     <main className="min-h-screen">
       <SparklesTitle title="About us" />
+      <LocationPin />
     </main>
   );
 }
