3b8b54372fd59f2cf85e6f2890938b788a1c2cfe
Viktor Miric <viktorm47@gmail.com>
2024-03-17 14:18:21 +0100

    Mobile menu

--- src/components/navbar/MobileMenu.tsx
+++ src/components/navbar/MobileMenu.tsx
@@ -1,7 +1,28 @@
+'use client';
+
+import { Menu } from 'lucide-react';
+import { useState } from 'react';
+import { Button } from '../ui/button';
+
 export default function MobileMenu() {
+  const [menu, setMenu] = useState(false);
+
   return (
-    <div className="sm:hidden">
-      <p>mobile</p>
-    </div>
+    <>
+      <div className="sm:hidden">
+        <Button
+          variant="ghost"
+          className="w-fit h-fit p-2"
+          onClick={() => setMenu((prev) => !prev)}
+        >
+          <Menu size={40} strokeWidth={1.5} />
+        </Button>
+      </div>
+      {menu && (
+        <>
+          <div className="absolute left-0 top-0 animate-in slide-in-from-top-full -z-50 h-screen w-full bg-secondary"></div>
+        </>
+      )}
+    </>
   );
 }
--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -8,7 +8,7 @@ export default function Navbar() {
   return (
     <nav className="w-full sticky top-0  z-50 transition">
       <TransparencyControl />
-      <div className="flex mx-auto max-w-screen-2xl items-center justify-between px-10 md:px-16 py-4">
+      <div className="flex mx-auto max-w-screen-2xl items-center justify-between px-3 sm:px-10 md:px-16 py-4">
         <Link href="/">
           <div className="flex items-center gap-2">
             <Image src="/logo.png" width={32} height={32} alt="Logo" />
