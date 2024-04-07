f375aeedadaf0d296ca00e93f844207e743e9a5d
Viktor Miric <viktorm47@gmail.com>
2024-04-07 18:20:00 +0200

    Fetching news data from CMS

--- src/components/home-sections/News.tsx
+++ src/components/home-sections/News.tsx
@@ -1,7 +1,17 @@
-export default function News() {
+export default async function News() {
+  const response = await fetch(`${process.env.CMS_URI}`, {
+    cache: 'no-store',
+    method: 'GET',
+    headers: {
+      authorization: `${process.env.API_KEY}`,
+    },
+  });
+  const data = await response.json();
+  console.log(data);
+
   return (
     <section className="">
-      <p>news</p>
+      <p>news123</p>
     </section>
   );
 }
