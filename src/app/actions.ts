67dbe1cff7c4b582336dd9bca1450d8805babc45
Viktor Miric <viktorm47@gmail.com>
2024-04-25 13:42:54 +0200

    Upgraded next.js version

--- src/app/actions.ts
+++
@@ -1,8 +0,0 @@
-'use server';
-
-import { revalidatePath, revalidateTag } from 'next/cache';
-
-export default async function revalidatePosts() {
-  revalidateTag('posts');
-  revalidatePath('/news');
-}
--- src/app/api/revalidate/route.ts
+++ src/app/api/revalidate/route.ts
@@ -1,9 +1,9 @@
-import revalidatePosts from '@/app/actions';
+import { revalidateTag } from 'next/cache';
 
 export async function GET(request: Request) {
   if (request.headers.get('authorization') !== process.env.API_KEY)
     return Response.json('Unauthorized.');
 
-  revalidatePosts();
+  revalidateTag('posts');
   return Response.json('Posts revalidated.');
 }
